858fee2c2e7a915ecf472d283af28dbe02bb6f31
Michał Kurnik <michal.kurnik@microsoft.wsei.edu.pl>
2022-04-02 23:24:44 +0200

    utworzenie klasy standart i override

---
+++ GradeBook/RankedGradeBook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60 && averageGrade < 80)
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade < 60)
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade < 40)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+    }
+}
+
